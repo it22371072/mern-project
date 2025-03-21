2970be6c896f4f9fb65eddc87d647798345e7eb0
Avishka27234 <IT22301072@my.sliit.lk>
2025-03-21 11:12:26 +0530

    create pages and routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
-export default function app() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import PasswordRst from './pages/PasswordRst'
+
+export default function App() {
   return (
-    <div>
-      app
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Sign-in" element={<Signin />} />
+        <Route path="/Sign-up" element={<Signup />} />
+        <Route path="/Password-Rst" element={<PasswordRst />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
+
